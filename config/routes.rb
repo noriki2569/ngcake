685b6b7a3bd26160cad8e88921ce030224c73e66
EC2 Default User <ec2-user@ip-172-31-14-123.ap-northeast-1.compute.internal>
2023-07-18 14:27:20 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+
+  resources :admin, only: [:top]
   root to: 'homes#top'
-  get 'homes/about'
+  get 'homes/about',to: 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  email: "noriki2569@gmail.com",
+  password: "1234567"
+  )
