4d841220958e545a0e84cd26a49fa19d615d61e2
EC2 Default User <ec2-user@ip-172-31-14-123.ap-northeast-1.compute.internal>
2023-07-22 10:23:08 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-
-  devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-
-  namespace :admin do
-     root to: 'homes#top'
-  end
-
+  
   scope module: :public do
     root to: 'homes#top'
     get 'about' => 'homes#about'
@@ -19,5 +7,17 @@ Rails.application.routes.draw do
     get 'customers/edit' => 'customers#edit'
     patch 'customers/update' => 'customers#update'
   end
+  
+  namespace :admin do
+     root to: 'homes#top'
+  end
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
