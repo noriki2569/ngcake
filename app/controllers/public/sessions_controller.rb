b03851faa01483f750ed1adc53d1063d14494f17
EC2 Default User <ec2-user@ip-172-31-14-123.ap-northeast-1.compute.internal>
2023-07-25 15:11:51 +0000

    update

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  #before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_starte, only: [:create]
 
   def after_sign_in_path_for(resource)
     root_path
@@ -25,8 +25,15 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
-
+   protected
+   def customer_state
+     @customer = Customer.find_by(email: params[:customer][:email])
+     return if !@customer
+     if @customer.valid_password?(params[:customer][:password]
+       is_deleted true && !false
+       redirect_to new_customer_registration_path
+     end
+   end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_16_045019) do
+ActiveRecord::Schema.define(version: 2023_07_25_144159) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_07_16_045019) do
     t.string "telephone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
