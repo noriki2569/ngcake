c996145edb6b4edbcccfa3eab394263becbd71fe
EC2 Default User <ec2-user@ip-172-31-14-123.ap-northeast-1.compute.internal>
2023-08-06 13:25:27 +0000

    update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,12 +3,17 @@ class Admin::CustomersController < ApplicationController
     @customers = Customer.all
   end
   def show
-    
+    @customer = Customer.find(params[:id])
   end
   def edit
-    
+    @customer = Customer.find(params[:id])
   end
   def update
-    
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,20 @@
 class Public::CartItemsController < ApplicationController
   def index
-
+    @item = Item.all
   end
+
   def update
 
   end
+
   def destroy
 
   end
+
   def destroy_all
 
   end
+
   def create
      @cart_items = CartItem.new(item_params)
     if @cart_items.save
